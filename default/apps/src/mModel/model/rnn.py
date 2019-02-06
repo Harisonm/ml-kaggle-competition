53c6aee9c83c4d96eb00ba54a36045bd513b04dd
Yasmiiiiine <yastouzene@gmail.com>
2019-02-06 10:58:49 +0100

    update yas_branch

--- default/apps/src/mModel/model/rnn.py
+++ default/apps/src/mModel/model/rnn.py
@@ -1,105 +1,112 @@
-# Simple CNN model for CIFAR-10
-import numpy
-from keras.datasets import cifar10
+from default.apps.src.mModel.manager.ModelManager import ModelManager
+from keras.layers import Dense
 from keras.models import Sequential
-from keras.layers import Dense, LSTM
-from keras.layers import Dropout
-from keras.constraints import maxnorm
-from keras.optimizers import SGD
-from keras import backend as K
-from keras.utils import np_utils
 from keras.callbacks import TensorBoard
-from default.apps.src.mModel.model.modelManager import ModelManager
-K.set_image_dim_ordering('th')
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.utils import np_utils
+import numpy
+
+PATH_TB = "./logsModel/tensorboard/"
+PATH_HISTORY = "./logsModel/history/"
 
 
 class Rnn(ModelManager):
 
     def __init__(self, param, dataset):
+        super().__init__(param, dataset)
+        self.__param = self._random_param(param)
+        self.__dataset = self._preprocess_cifar10(dataset)
 
-        ModelManager.__init__(self, param, dataset)
-        self.__dataset = self.preprocess_cifar10(self)
-        self.__param = self.random_param(param)
-
-    def run_model_rnn(self):
-
-        # load data
+    def run_model(self):
+        '''
+        :return:
+        '''
         (X_train, y_train), (X_test, y_test) = self.__dataset
-        # normalize inputs from 0-255 to 0.0-1.0
-        X_train = X_train.astype('float32')
-        X_test = X_test.astype('float32')
-        X_train = X_train / 255.0
-        X_test = X_test / 255.0
-
-        # one hot encode outputs
-        y_train = np_utils.to_categorical(y_train)
-        y_test = np_utils.to_categorical(y_test)
         nb_classes = y_test.shape[1]
-
-        # Compile model
-        decay = self.__param['lr'] / self.__param['epochs']
+        type_model = "rnn"
 
         # Create the model
         model = Sequential()
 
         model.add(LSTM(300,
-                       input_shape=self.__param['input_shape'],
-                       return_sequences=True))
-        model.add(Dropout(self.__param['dropout']))
-        model.add(LSTM(300))
-        model.add(Dropout(self.__param['dropout']))
-        model.add(Dense(self.__param['"layerParam")[''denseMiddle'],
-                        activation=self.__param['activation'],
-                        kernel_constraint=self.__param['kernel_constraint']))
+                       input_shape=(1024, 3),
+                       activation=self.__param['activation'],
+                       kernel_constraint=self.__param['kernel_constraint'],
+                       return_sequences=True
+                       ))
 
-        print('Compiling...')
+        model.add(Dropout(self.__param['dropout']))
+
+        model.add(LSTM(300,
+                       activation=self.__param['activation'],
+                       kernel_constraint=self.__param['kernel_constraint']))
+
+        model.add(Dropout(self.__param['dropout']))
+
+        model.add(Dense(nb_classes,
+                        activation=self.__param['last_activation']))
+
         model.compile(loss=self.__param['losses'],
                       optimizer=self.__param['optimizer'],
-                      metrics=['accuracy'])
-
-        # model.add(Conv2D(32, (3, 3),
-        #                  input_shape=self.__param['input_shape'],
-        #                  padding=self.__param['padding'],
-        #                  activation=self.__param['activation'],
-        #                  kernel_constraint=self.__param['kernel_constraint']))
-        #
-        # model.add(Dropout(self.__param['dropout']))
-        #
-        # model.add(Conv2D(32, (3, 3),
-        #                  activation=self.__param['activation'],
-        #                  padding=self.__param['padding'],
-        #                  kernel_constraint=self.__param['kernel_constraint']))
-        #
-        # model.add(MaxPooling2D(pool_size=(2, 2)))
-        #
-        # model.add(Flatten())
-        #
-        # model.add(Dense(self.__param['"layerParam")[''denseMiddle'],
-        #                 activation=self.__param['activation'],
-        #                 kernel_constraint=self.__param['kernel_constraint']))
-        #
-        # model.add(Dropout(self.__param['dropout']))
-        #
-        # model.add(Dense(nb_classes,
-        #                 activation=self.__param['activation']))
-        #
-        # model.compile(loss=self.__param['losses'],
-        #               optimizer=self.__param['optimizer'],
-        #               metrics=['accuracy'])
-
+                      metrics=self.__param['metrics'])
         model.summary()
 
-        type_model = "rnn"
-        tb_callback = self.save_tensorboard(model, type_model)
+        tb_callback = self.__save_tensorboard(model, type_model)
+        # training
+        history = model.fit(X_train, y_train,
+                            batch_size=self.__param['batch_size'],
+                            epochs=self.__param['epochs'],
+                            verbose=1,
+                            validation_data=(X_test, y_test),
+                            callbacks=[tb_callback])
 
-        # Fit the model
-        print('Fitting...')
-        model.fit(X_train, y_train,
-                  validation_data=(X_test, y_test),
-                  epochs=self.__param['epochs'],
-                  batch_size=self.__param['batch_size'],
-                  callbacks=[tb_callback])
+        self._save_history(history, PATH_HISTORY + type_model + "/" + str(self.__param['activation']) + "_" +
+                           str(self.__param['losses']) + str(self.__param['metrics']) + "_" + 'history.txt')
 
-        # Final evaluation of the model
-        scores = model.evaluate(X_test, y_test, verbose=0)
-        print("Accuracy: %.2f%%" % (scores[1]*100))
+        score = model.evaluate(X_test, y_test, verbose=1)
+        print('test loss:', score[0])
+        print('test acc:', score[1])
+
+        self._run_ml_flow(self.__param, history, model, score)
+        return history, model
+
+    def __save_tensorboard(self, model, type_model):
+        model_str = type_model + "_" + \
+                    str(self.__param['epochs']) + "_" + \
+                    str(self.__param['batch_size']) + "_" + \
+                    str(self.__param['activation']) + "_" + \
+                    str(self.__param['losses']) + "_" + \
+                    str(self.__param['optimizer'])
+
+        model.save(PATH_TB + type_model + "/saved_models" + "_" + model_str, True, True)
+
+        # Save tensorboard callback
+        tb_callback = TensorBoard(log_dir=str(PATH_TB) + "/" + type_model + "/" + type_model + "_" +
+                                          str(self.__param['epochs']) + "_" +
+                                          str(self.__param['batch_size']) + "_" +
+                                          str(self.__param['activation']) + "_" +
+                                          str(self.__param['losses']) + "_" +
+                                          str(self.__param['optimizer']))
+        return tb_callback
+
+    def _preprocess_cifar10(self, dataset):
+        """
+        :param dataset:
+        :return:
+        """
+        (__X_train, __y_train), (__X_test, __y_test) = dataset
+        # normalize inputs from 0-255 to 0.0-1.0
+        __X_train = __X_train.astype('float32')
+        __X_test = __X_test.astype('float32')
+        __X_train = __X_train / 255.0
+        __X_test = __X_test / 255.0
+
+        # one hot encode outputs
+        __y_train = np_utils.to_categorical(__y_train)
+        __y_test = np_utils.to_categorical(__y_test)
+
+        # reshape X
+        __X_train = numpy.reshape(__X_train, (len(__X_train), 1024, 3))
+        __X_test = numpy.reshape(__X_test, (len(__X_test), 1024, 3))
+        return (__X_train, __y_train), (__X_test, __y_test)
