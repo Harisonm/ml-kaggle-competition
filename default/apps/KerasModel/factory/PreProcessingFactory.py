989d4edd34889ccbeaf94000863adab9d9fa97ae
Manitra RANAIVOHARISON <manitra.harison@gmail.com>
2019-05-05 13:06:36 +0200

    [manitra_kaggle_competion]: (updating) -> save

---
+++ default/apps/KerasModel/factory/PreProcessingFactory.py
@@ -0,0 +1,14 @@
+import mlflow
+import mlflow.keras
+from tensorflow.python import keras
+import os
+import sys
+
+
+class PreProcessingFactory(object):
+
+    def __init__(self):
+        pass
+
+    def _pre_processing_dataset(self):
+        pass
